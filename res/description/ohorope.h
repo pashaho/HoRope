a037c1227b0fd547b7f9b604e2d13078b60f6be1
pashaho <34800779+pashaho@users.noreply.github.com>
2018-01-31 16:37:56 +0300

    Add files via upload

---
+++ res/description/ohorope.h
@@ -0,0 +1,17 @@
+#ifndef _Ohorope_H_
+#define _Ohorope_H_
+
+enum
+{
+    Ohorope         = 1040416,
+
+    HOROPE_RES      = 1001,
+    HOROPE_RAD      = 1002,
+    HOROPE_FSUB     = 1003,
+    HOROPE_SEG      = 1004,
+    HOROPE_CTRL     = 1005,
+    HOROPE_SKIN      = 1006,
+    HOROPE_SKIN_ON   = 1007,
+};
+
+#endif
